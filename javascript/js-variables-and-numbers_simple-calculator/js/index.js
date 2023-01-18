188fa4d67d35d51df8bdedf198067bb178f8e90c
nmarforio <n.marforio@gmail.com>
2023-01-18 11:02:17 +0100

    Challenges done

---
+++ javascript/js-variables-and-numbers_simple-calculator/js/index.js
@@ -0,0 +1,109 @@
+console.clear();
+
+let operand1 = 12;
+const operand2 = 4;
+
+/*
+Mathematical operations
+*/
+const addButton = document.querySelector('[data-js="add"]');
+const subtractButton = document.querySelector('[data-js="subtract"]');
+const multiplyButton = document.querySelector('[data-js="multiply"]');
+const divideButton = document.querySelector('[data-js="divide"]');
+const exponentButton = document.querySelector('[data-js="exponent"]');
+const moduloButton = document.querySelector('[data-js="modulo"]');
+
+addButton.addEventListener("click", () => {
+  const addResult = operand1 + operand2;
+  console.log(addResult);
+  // 1. Add the two operands and store the result in a variable. Log the variable's value to the console.
+});
+
+subtractButton.addEventListener("click", () => {
+  const substractResult = operand1 - operand2;
+  console.log(substractResult);
+  // 2. Subtract the two operands and store the result in a variable. Log the variable's value to the console.
+});
+
+multiplyButton.addEventListener("click", () => {
+  const multiplyResult = operand1 * operand2;
+  console.log(multiplyResult);
+
+  // 3. Multiply the two operands and store the result in a variable. Log the variable's value to the console.
+});
+
+divideButton.addEventListener("click", () => {
+  const divideResult = operand1 / operand2;
+  console.log(divideResult);
+  // 4. Divide the two operands and store the result in a variable. Log the variable's value to the console.
+});
+
+exponentButton.addEventListener("click", () => {
+  const exponentreuslt = operand1 ** operand2;
+  console.log(exponentreuslt);
+  // 5. Raise "operand1" to the value of "operand2" and store the result in a variable. Log the variable's value to the console.
+});
+
+moduloButton.addEventListener("click", () => {
+  const moduloresult = operand1 % operand2;
+  console.log(moduloresult);
+  // 6. Calculate the remainder when dividing "operand1" by "operand2" and store the result in a variable. Log the variable's value to the console.
+});
+
+/*
+Update the first operand
+
+Please don't forget to think about the declaration of the variable "operand1".
+*/
+const increaseByOneButton = document.querySelector(
+  '[data-js="increase-by-one"]'
+);
+const increaseByFiveButton = document.querySelector(
+  '[data-js="increase-by-five"]'
+);
+const decreaseByOneButton = document.querySelector(
+  '[data-js="decrease-by-one"]'
+);
+const decreaseByFiveButton = document.querySelector(
+  '[data-js="decrease-by-five"]'
+);
+const multiplyByTwoButton = document.querySelector(
+  '[data-js="multiply-by-two"]'
+);
+const divideByTwoButton = document.querySelector('[data-js="divide-by-two"]');
+
+increaseByOneButton.addEventListener("click", () => {
+  const increasing1 = (operand1 += 1);
+  console.log(increasing1);
+  // 7. Increase the value of "operand1" by 1. Log the new value to the console.
+});
+
+increaseByFiveButton.addEventListener("click", () => {
+  const increasing5 = (operand1 += 5);
+  console.log(increasing5);
+  // 8. Increase the value of "operand1" by 5. Log the new value to the console.
+});
+
+decreaseByOneButton.addEventListener("click", () => {
+  const decrease1 = (operand1 -= 1);
+  console.log(decrease1);
+  // 7. Decrease the value of "operand1" by 1. Log the new value to the console.
+});
+
+decreaseByFiveButton.addEventListener("click", () => {
+  const decrease5 = (operand1 -= 5);
+  console.log(decrease5);
+  // 8. Decrease the value of "operand1" by 5. Log the new value to the console.
+});
+
+multiplyByTwoButton.addEventListener("click", () => {
+  const multiplyBy2Result = operand1 * 2;
+  console.log(multiplyBy2Result);
+  // 11. Multiply the value of "operand1" by 2. Log the new value to the console.
+});
+
+divideByTwoButton.addEventListener("click", () => {
+  const divideBy2Result = operand1 / 2;
+  console.log(divideBy2Result);
+  // 12. Divide the value of "operand1" by 2. Log the new value to the console.
+});
---
+++ javascript/js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,33 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+
+*/
+
+// --v-- write your code here --v--
+const title = "holidays";
+const text = "Drinking at the beach";
+let likes = 56;
+const user = "Alice B.";
+const isReported = true;
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+
+console.log(title);
+console.log(text);
+console.log((likes += 1));
+console.log(user);
+console.log(isReported);
+
+// --^-- write your code here --^--
