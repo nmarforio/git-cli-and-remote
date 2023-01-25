070ecb93cb4e2820e92dbcb8f3e75ae277b6828f
nmarforio <n.marforio@gmail.com>
2023-01-25 17:27:23 +0100

    challenges controlled

--- javascript/js-forms-2_simple-live-calculator/js/index.js
+++ javascript/js-forms-2_simple-live-calculator/js/index.js
@@ -7,17 +7,16 @@ const result = document.querySelector('[data-js="result"]');
 function multiply(num1, num2) {
   return num1 * num2;
 }
-
-firstInput.addEventListener("input", () => {
+const eventHendler = () => {
   const numbers = firstInput.value;
   const realNumbers1 = parseInt(numbers);
-  return realNumbers1;
-});
 
-secondInput.addEventListener("input", () => {
   const numbers2 = secondInput.value;
   const realNumbers2 = parseInt(numbers2);
-  return realNumbers2;
-});
 
-result.textContent = multiply(realNumbers1, realNumbers2);
+  result.textContent = multiply(realNumbers1, realNumbers2);
+};
+
+firstInput.addEventListener("input", eventHendler);
+
+secondInput.addEventListener("input", eventHendler);
--- javascript/js-forms-2_simple-todolist/js/index.js
+++ javascript/js-forms-2_simple-todolist/js/index.js
@@ -7,7 +7,7 @@ const headline = document.querySelector("headline");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const formData = new FormData(event.target);
+  const formData = new FormData(event.target); //or form
   const data = Object.fromEntries(formData);
   console.log(data);
 
@@ -17,14 +17,14 @@ form.addEventListener("submit", (event) => {
   newlist(data);
 });
 
-function newlist(data) {
-  // the data is an object, that's why for textcontent we do data.headline
+function newlist(tododata) {
+  // the tododata is an object, that's why for textcontent we do tododata.headline
   const list = document.createElement("li");
   todoList.append(list); //to reach a ul or a deep element in the body is better to create a queryselector
   const headlineHTML = document.createElement("h3");
   const taskHTML = document.createElement("p");
   list.append(headlineHTML, taskHTML);
 
-  headlineHTML.textContent = data.headline;
-  taskHTML.textContent = data.task;
+  headlineHTML.textContent = tododata.headline; //tododata.headline because the data object {data}
+  taskHTML.textContent = tododata.task;
 }
