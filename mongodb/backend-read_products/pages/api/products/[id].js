e5177f1afd2e89ba2370f6d0eabcc839e0be716a
nmarforio <n.marforio@gmail.com>
2023-02-24 22:10:12 +0100

    mongodb

---
+++ mongodb/backend-read_products/pages/api/products/[id].js
@@ -0,0 +1,16 @@
+//import { products } from "../../../lib/products";
+import dbConnect from "../../../db/connect";
+import Product from "../../../db/models/Product";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  const productid = await Product.findById(id);
+
+  if (!productid) {
+    return response.status(404).json({ status: "Not Found" });
+  }
+
+  response.status(200).json(productid);
+}
---
+++ mongodb/backend-read_products/pages/api/products/index.js
@@ -0,0 +1,13 @@
+// import { products } from "../../../lib/products";
+import dbConnect from "../../../db/connect";
+import Product from "../../../db/models/Product";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const products = await Product.find();
+    console.log(products);
+    return response.status(200).json(products);
+  }
+}
